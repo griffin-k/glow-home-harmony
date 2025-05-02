85b9d76b519f331251959b13f7d3fb553f411a45
Karimullah <karimullah.khan3637@gmail.com>
2025-05-02 12:44:27 +0500

    updated

--- src/pages/Lights.tsx
+++ src/pages/Lights.tsx
@@ -28,7 +28,7 @@ const Lights = () => {
 
   const lightsList = [
     { id: "Light1", name: "Light 1" },
-    { id: "Light2", name: "Light 2" },
+    { id: "fan1", name: "Light 2" },
     { id: "Light3", name: "Light 3" },
     { id: "Light4", name: "Light 4" },
   ];
